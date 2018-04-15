79931e1172b8f0d3ea61be17eb1d5d8eecd0a517
vito <vitogit@users.noreply.github.com>
2018-04-14 21:39:49 -0300

    Add location and descriptions

--- src/eventGenerator.js
+++ src/eventGenerator.js
@@ -84,6 +84,143 @@ let subjects = [
 'Clima',
 ]
 
+let places = [
+  'Lago',
+  'Calle',
+  'Barrio pobre',
+  'Barrio rico',
+  'Bosque',
+  'Ruinas',
+  'Parque',
+  'Colinas',
+  'Casa',
+  'Puente',
+  'Centro',
+  'Cueva',
+  'Campamento',
+  'Campo',
+  'Lago',
+  'Pantano',
+  'Fabrica',
+  'Empresa',
+  'Oficina',
+  'Cementerio',
+  'Laboratorio',
+  'Hospital',
+  'Academia',
+  'Apartamento',
+  'Mercado',
+  'Parada',
+  'Iglesia',
+  'Monumento',
+  'Libreria',
+  'Museo',
+  'Galeria',
+  'Teatro',
+  'Restaurant',
+  'Tienda',
+  'Aeropuerto',
+  'Banco',
+  'Subte',
+]
+
+let adjective = [
+'Aburrido',
+'Alegre',
+'Alto',
+'Bajo',
+'Claro',
+'Oscuro',
+'Caliente',
+'Frio',
+'Bueno',
+'Malo',
+'Duro',
+'Blando',
+'Fuerte',
+'Debil',
+'Facil',
+'Dificil',
+'Exterior',
+'Interior',
+'Falso',
+'Fresco',
+'Grande',
+'Chico',
+'Humedo',
+'Seco',
+'Imosible',
+'Util',
+'Inutil',
+'Joven',
+'Viejo',
+'Antiguo',
+'Lento',
+'Rapido',
+'Masivo',
+'Diminuto',
+'Vivo',
+'Muerto',
+'Natural',
+'Pobre',
+'Rico',
+'Pobre',
+'Raro',
+'Extranio',
+'Real',
+'Brillante',
+'Palido',
+'Sano',
+'Enfermo',
+'Corrupto',
+'Aislado',
+'Triste',
+'Desaolado',
+'Verde',
+'Anormal',
+'Abandonado',
+'Colosal',
+'Oculto',
+'Siniestro',
+'Escondido',
+'Complejo',
+'Simple',
+'Derruido',
+'Roto',
+'Desolado',
+'Asqueroso',
+'Calmo',
+'Abierto',
+'Hermoso',
+'Sucio',
+'Limpio',
+'Clasico',
+'Espeluznante',
+'Normal',
+'Delicado',
+'Desagradable',
+'Vacio',
+'Exotico',
+'Familiar',
+'Historico',
+'Horrible',
+'Valioso',
+'Importante',
+'Podido',
+'Divertido',
+'Aromatico',
+'Luminoso',
+'Macabro',
+'Glorioso',
+'Peligroso',
+'Pacifico',
+'Agresivo',
+'Calmo',
+'Festivo',
+'Eterno',
+'Majestuoso',
+'Imponente',
+]
 function randArray(myArray) {
  return myArray[Math.floor(Math.random() * myArray.length)];
 }
@@ -110,6 +247,27 @@ export function complexQuestion() {
 
   var el =  "";
   el += "<b>Respuesta:</b> "+action+"/"+subject+"<br>";
+
+  return el;
+}
+
+export function place() {
+
+  var a1 = randArray(places);
+
+  var el =  "";
+  el += "<b>Lugar:</b> "+a1+"<br>";
+
+  return el;
+}
+
+export function complexDescription() {
+
+  var a1 = randArray(adjective);
+  var a2 = randArray(adjective);
+
+  var el =  "";
+  el += "<b>Descripcion:</b> "+a1+"/"+a2+"<br>";
 
   return el;
 }
